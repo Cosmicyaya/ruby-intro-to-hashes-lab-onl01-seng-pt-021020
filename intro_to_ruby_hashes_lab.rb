4b1f3c73a1c14ccdfa454fee1048ac8d57339c0c
Ryan Hollins <ballbaroi@gmail.com>
2020-02-22 14:43:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads = {pieces: 4, names{}, rent_in_dollars
+	  :railroads = {pieces: 4, names{}, rent_in_dollars {}}
 	}
 
 def monopoly_with_third_tier
